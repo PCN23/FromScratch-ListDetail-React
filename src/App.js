3a88ddf4bd5deb1f44d821fc38610a0a6e34a059
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-06-24 15:59:18 -0700

    3 links showing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from 'react-router-dom';
-import HorsesPage from './HorsesPage.js';
 import HorsesDetail from './HorsesDetail.js';
+import HorsesPage from './HorsesPage';
 
 export default function App() {
   return (
     <Router>
-      <div className="App">
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
         <Switch>
-          <Route>
-            Hello
+          <Route exact path="/about">
+            <HorsesPage />
           </Route>
-          <Route>
-            Hello
+          <Route exact path="/detail-page">
+            <HorsesDetail />
           </Route>
+          
         </Switch>
       </div>
     </Router>
   );
 }
-
-export default App;
